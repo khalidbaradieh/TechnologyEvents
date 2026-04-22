a5143f1e0fbdee247086b3033876542d66220c0e
khalidbaradieh <khalid.baradia@gmail.com>
2026-04-23 00:49:58 +0300

    updt

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,17 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: 'src/index.html',
+        index: 'src/index.html',
         admin: 'src/admin.html',
         rbac: 'src/rbac.html'
+      },
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
       }
-    }
+    },
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
